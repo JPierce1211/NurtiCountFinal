10e93946e385e85d020e9d3ed80de63ec7b3b221
Juan Leonard <juanzleonard012@gmail.com>
2023-08-08 17:59:35 -0400

    Issues fixing

--- java/src/main/java/com/techelevator/controller/ProgressController.java
+++ java/src/main/java/com/techelevator/controller/ProgressController.java
@@ -2,12 +2,14 @@ package com.techelevator.controller;
 
 import com.techelevator.dao.ProfileDao;
 import com.techelevator.dao.ProgressDao;
+import com.techelevator.exception.DaoException;
 import com.techelevator.model.Profile;
 import com.techelevator.model.Progress;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -33,15 +35,19 @@ public class ProgressController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("profile/{id}/create-process")
-    public Progress create(Progress progress, @PathVariable int profileId){
-        Profile profile = profileDao.getProfileById(profileId);
-        Progress newProgress = progressDao.createProgress(progress);
+    public Progress create(@RequestBody Progress progress, @PathVariable int profileId){
+        progress.setProfileId(profileId);
+        try {
+            Progress newProgress = progressDao.createProgress(progress);
+            return newProgress;
+        } catch (DaoException e){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Reservation not found.");
+        }
 
-        return null;
     }
 
     @PutMapping("profile/{id}/progress/update")
-    public Progress update(Progress progress){
+    public Progress update(@RequestBody Progress progress){
         return progressDao.updateProgress(progress);
     }
 
