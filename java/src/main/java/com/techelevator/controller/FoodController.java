b7bfceefde14647dacd0d8d2affe52af68792ee2
Delayne Stewart Jr <dstewartjr1@gmail.com>
2023-08-08 11:08:51 -0400

    food class

--- java/src/main/java/com/techelevator/controller/FoodController.java
+++ java/src/main/java/com/techelevator/controller/FoodController.java
@@ -1,8 +1,15 @@
 package com.techelevator.controller;
 
+import com.techelevator.dao.FoodDao;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class FoodController
-{
+@RequestMapping
+public class FoodController {
+    private FoodDao foodDao;
+
+    public FoodController(FoodDao foodDao) {
+        this.foodDao = foodDao;
+    }
 }
