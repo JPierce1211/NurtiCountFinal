4ca2e3094d58f06590015bc2e4ddfbf796d054a2
Juan Leonard <juanzleonard012@gmail.com>
2023-08-09 13:24:29 -0400

    Smaller edits on the profile and progress classes

--- java/src/main/java/com/techelevator/controller/ProfileController.java
+++ java/src/main/java/com/techelevator/controller/ProfileController.java
@@ -2,12 +2,14 @@ package com.techelevator.controller;
 
 import com.techelevator.dao.ProfileDao;
 import com.techelevator.dao.UserDao;
+import com.techelevator.exception.DaoException;
 import com.techelevator.model.Profile;
 import com.techelevator.model.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.security.Principal;
 
@@ -35,9 +37,15 @@ public class ProfileController {
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/profile/{id}")
     public Profile create(@RequestBody Profile profile, @PathVariable int userId, Principal principal){
-        Profile userIdFromProfile = profileDao.getProfileByUserId(userId);
-        userIdFromProfile.setUserId(userId);
-        Profile newProfile = profileDao.createProfile(profile);
+        Profile newProfile = null;
+        User user = userDao.getUserByUsername(principal.getName());
+        try {
+            if (user.getId() == userId){
+                 newProfile = profileDao.createProfile(profile);
+            }
+        } catch (DaoException e){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Reservation not found.");
+        }
         return newProfile;
     }
 
